eaa61573de71c44de95d5ef90d78309ce6a567aa
Andreas Baumgartner <mail@andreas.bz.it>
2022-12-06 15:19:06 +0100

    [day 6] solve puzzle °12

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ I'm participating just for fun, so don't expect particularly clever solutions.
 - Day 3: Rucksack Reorganization *(both parts)*
 - Day 4: Camp Cleanup *(both parts)*
 - Day 5: Supply Stacks *(both parts)*
+- Day 6: Tuning Trouble *(both parts)*
--- day06/solutions.py
+++ day06/solutions.py
@@ -1,8 +1,20 @@
 with open("input.txt", "r") as inputFile:
     byteStream = inputFile.read()
-    chunkSize = 4
+    packetMarkerLength = 4
+    messageMarkerLength = 14
+    solutionPart1 = 0
+    solutionPart2 = 0
     for i in range(0, len(byteStream)):
-        fourMostRecentlyReceivedCharacters = byteStream[i : i + chunkSize]
-        if len(set(fourMostRecentlyReceivedCharacters)) > 3:
-            print(f"Solution part 1: {i+chunkSize}")
-            break
+        if solutionPart1 == 0:
+            fourMostRecentlyReceivedCharacters = byteStream[i : i + packetMarkerLength]
+            if len(set(fourMostRecentlyReceivedCharacters)) == packetMarkerLength:
+                solutionPart1 = packetMarkerLength + i
+
+        if solutionPart2 == 0:
+            fourteenMostRecentlyReceivedCharacters = byteStream[i : i + messageMarkerLength]
+            if len(set(fourteenMostRecentlyReceivedCharacters)) == messageMarkerLength:
+                solutionPart2 = messageMarkerLength + i
+
+
+    print(f"Solution part 1: {solutionPart1}")
+    print(f"Solution part 2: {solutionPart2}")
